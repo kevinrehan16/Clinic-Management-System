ec6c9a383b3a60392ee24a58fe81ad2bdc3a603d
Kevin <kevinrehan16@gmail.com>
2026-07-01 10:48:43 +0800

    FIXED: Added a validation of past date and doctors schedule for the appointment.

--- backend/apps/appointments/serializers/appointment_serializers.py
+++ backend/apps/appointments/serializers/appointment_serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.utils import timezone
+from datetime import date
 from appointments.models import Appointment
 from doctors.models import DoctorProfile
 
@@ -12,3 +14,29 @@ class AppointmentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = ['doctor_id', 'appointment_date', 'appointment_time', 'reason_for_visit']
+
+    
+    def validate(self, data):
+        """
+        Dito ginagawa ang Object-level validation.
+        Tumatakbo ito pagkatapos ng field-level validation.
+        """
+        today = timezone.now().date()
+
+        doctor = data.get('doctor')
+        date = data.get('appointment_date')
+
+        # 1. Validation: Hindi pwedeng mag-book sa nakalipas na oras
+        if date < today:
+            raise serializers.ValidationError({
+                "appointment_date": "Hindi maaaring mag-book sa petsa o oras na nakalipas na."
+            })
+
+        # 2. Validation: Check kung may conflict (Double Booking)
+        # Tinitignan natin kung may existing appointment na ang doctor sa oras na ito
+        if Appointment.objects.filter(doctor=doctor.user, appointment_date=date).exists():
+            raise serializers.ValidationError({
+                "appointment_date": "Ang doctor ay may appointment na sa oras na ito. Pakipili ng ibang schedule."
+            })
+
+        return data
